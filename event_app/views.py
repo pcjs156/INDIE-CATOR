ce8996766f3154e8f2a171e0929c2dc4198d2140
Yubin_Park on Window <qkrdbqls1001@naver.com>
2020-05-28 00:05:13 +0900

    event_detail에 댓글 목록 만들었음

--- event_app/views.py
+++ event_app/views.py
@@ -9,4 +9,6 @@ def home(request):
 # 공연 세부 정보 : /event_detail/(event_id)
 def event_detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
-    return render(request, 'event_detail.html', {'event':event})
+    comments = Comment.objects.filter(event_id=event_id)
+
+    return render(request, 'event_detail.html', {'event':event, 'comments':comments})
